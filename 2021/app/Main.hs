8a5858378ebf7dcc10ce52bdd34e8e423f828ecc
Morten Torgund Dalfoss <mordal@erst.dk>
2021-12-05 19:28:00 +0100

    solved second puzzle of day 1

--- 2021/app/Main.hs
+++ 2021/app/Main.hs
@@ -3,7 +3,10 @@ module Main where
 main :: IO ()
 main = do
  d1_0 <- readFile "./inputs/input-1_0"
- putStrLn $ show $ puzzle1_0 . map read $ lines d1_0
+ let puzzle1Input = map read $ lines d1_0
+ putStrLn $ show $ puzzle1_0 puzzle1Input
+ putStrLn $ show $ puzzle1_1 puzzle1Input
+
 
 puzzle1_0 :: [Int] -> Int
 puzzle1_0 xs = go (head xs) (tail xs) 0 where
@@ -15,3 +18,12 @@ puzzle1_0 xs = go (head xs) (tail xs) 0 where
 
 puzzle1_0_1 :: [Int] -> Int
 puzzle1_0_1 xs = length . filter (\bool -> bool == True) $ zipWith (<) (xs) (tail xs)
+
+movingSum :: Int -> [Int] -> [Int]
+movingSum window lst = reverse $ drop (window - 1) $ go window lst [] where
+  go w l res = case l of
+    [] -> res
+    (_:_) -> go w (tail l) $ (sum . take w $ l) : res
+
+puzzle1_1 :: [Int] -> Int
+puzzle1_1 lst = (puzzle1_0 . movingSum 3) lst
